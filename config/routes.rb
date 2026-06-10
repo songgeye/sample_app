5c6f0ad7a31fbcb60a641f353fa9277f1881aa53
songgeye <79154673+songgeye@users.noreply.github.com>
2026-06-10 15:48:54 +0000

    タイトルのテストを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
-  root "application#hello"
+  get  'static_pages/about'
+  root 'application#hello'
 end
